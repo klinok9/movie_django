48a51568f100be20c9424efbdef2029b1138c6ff
klinok9 <0Dbhecyfqlty>
2020-05-29 11:05:08 +0300

    v1

--- movies/admin.py
+++ movies/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Category,Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
+from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
-# Register your models here.
+
 admin.site.register(Category)
 admin.site.register(Genre)
 admin.site.register(Movie)
--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
+
 from .models import Movie
 
-class MoviesView(View):
-    def get(self,request):
-        movies = Movie.objects.all()
-        return render(request, 'movies/movies.html', {'movie_list': movies})
+
+class MoviesView(ListView):
+    """Список фильмов"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+
+
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
+    model = Movie
+    slug_field = "url"
